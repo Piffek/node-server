ebdacaa32037cb03fb0bf5d83bf4e669c1b0962d
Piffek <patrykpiwko123412@gmail.com>
2017-06-10 19:52:52 +0200

    response

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ var http = require('http');
 
 function onRequest(request, response){
 	console.log(request.url);
+	response.writeHead(200, {'Context-Type': 'text/plain'});
+	response.write('response');
+	response.end();
 }
 
-http.createServer(onRequest).listen(8888);
+http.createServer(onRequest).listen(888);
 console.log('RUN');
