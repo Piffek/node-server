875564fd48d87f4669479b6c7bc8ff9d1a25d206
Piffek <patrykpiwko123412@gmail.com>
2017-06-10 20:28:15 +0200

    response

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 var http = require('http');
+var fileSystem = require('fs');
+
+function send404Response(response){
+	response.writeHead(404, {'Content-Type': 'text/plain'});
+	response.write('Error 404');
+	response.end();
+}
+
 
 function onRequest(request, response){
-	console.log(request.url);
-	response.writeHead(200, {'Context-Type': 'text/plain'});
-	response.write('response');
-	response.end();
+	
+	if(request.method == 'GET' && request.url == '/'){
+		response.writeHead(200, {'Content-Type': 'text/html'});
+		fileSystem.createReadStream(__dirname + '/index.html').pipe(response);
+	
+	}else{
+		send404Response(response);
+	}
 }
 
 http.createServer(onRequest).listen(888);
